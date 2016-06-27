4f80efc5506dce11537ec6e5b3c7de640075588c
Sashko Stubailo <sashko@stubailo.com>
2016-06-27 13:19:14 -0700

    Get to a reasonable failing state

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,10 @@ import {
   addTypenameToSelectionSet,
 } from './queries/queryTransform';
 
+import {
+  MutationApplyResultAction,
+} from './data/mutationResultActions';
+
 import isUndefined = require('lodash.isundefined');
 
 export {
@@ -104,6 +108,7 @@ export default class ApolloClient {
 
   public mutate = (options: {
     mutation: Document,
+    applyResult?: MutationApplyResultAction[],
     variables?: Object,
   }): Promise<GraphQLResult> => {
     this.initStore();
